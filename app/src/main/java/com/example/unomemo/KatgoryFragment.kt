e08a5f3da8ae604e42fa0e9799990f33edee4ece
unknown <svenis14@hotmail.com>
2021-09-30 15:12:52 +0200

    'initial'

---
+++ app/src/main/java/com/example/unomemo/KatgoryFragment.kt
@@ -0,0 +1,19 @@
+package com.example.unomemo
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [KatgoryFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class KatgoryFragment : Fragment(R.layout.fragment_katgory) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
