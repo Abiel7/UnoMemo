b6d85c38b026f72afbff2406678de67c2dee3602
232578 <70134656+232578@users.noreply.github.com>
2021-10-01 00:35:18 +0200

    new repo cleen initial

---
+++ app/src/main/java/com/example/unomemo/EntryPointFragment.kt
@@ -0,0 +1,41 @@
+package com.example.unomemo
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import com.example.unomemo.databinding.FragmentEntryBinding
+
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [EntryPointFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class EntryPointFragment : Fragment() {
+    lateinit var binding : FragmentEntryBinding
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil
+            .inflate(inflater, R.layout.fragment_entry, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.buttonToLoginActivity.setOnClickListener{
+            findNavController().navigate(R.id.action_entryPointFragment_to_loginFragment)
+        }
+        binding.skipGroup.setOnClickListener {
+            findNavController().navigate(R.id.action_entryPointFragment_to_katgoryFragment)
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/unomemo/login/LoginResult.kt
@@ -0,0 +1,11 @@
+package com.example.unomemo.login
+
+import com.example.unomemo.login.LoggedInUserView
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
