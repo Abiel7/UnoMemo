89fc1800eff4532ddd9833d9307e1e62a364ca84
unknown <svenis14@hotmail.com>
2021-09-30 15:12:52 +0200

    'initial'

---
+++ app/src/main/java/com/example/unomemo/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.unomemo
+
+import android.os.Bundle
+import android.text.BoringLayout
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.FragmentContainerView
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.example.unomemo.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var fragLayout:FragmentContainerView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = DataBindingUtil
+            .setContentView<ActivityMainBinding>(this,R.layout.activity_main)
+
+        fragLayout = binding.fragmentContainer
+
+        val navController = findNavController(R.id.nav_host_fragment)
+
+        //TODO add drawerLayout to main activity, wrap fragmentConteinerView
+        //NavigationUI.setupActionBarWithNavController(this,navController,fragLayout)
+
+        //TODO add NavigationView (matirial) bottom of layout
+        //NavigationUI.setupWithNavController(binding.navView,navController)
+
+    }
+}
