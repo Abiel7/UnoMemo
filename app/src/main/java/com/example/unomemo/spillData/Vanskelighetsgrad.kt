f61acc1ecba49ada1b718afea09e5812c1da74f4
abiel <abielkdiane19@hotmail.com>
2021-10-09 18:36:45 +0200

    fikset ferdig dataclass som inneholder info om kortene, laget enums for vanskelighets grad lagde en margin en klasse for spacing recycler view lagt inn 8 nye bilder

---
+++ app/src/main/java/com/example/unomemo/spillData/Vanskelighetsgrad.kt
@@ -0,0 +1,25 @@
+package com.example.unomemo.spillData
+
+enum class Vanskelighetsgrad(val antallkort:Int) {
+ENKEL(8), MIDDELS(12), KRVENDE(16);
+
+    // info om lengde og bredde tilsvarende til hver enkelt brett
+    // sånn når bruker endrer vanskelighetes grad så endres brette også
+    //tilsvarende til Vanskelighetsgrad
+
+    fun getBredde() :Int{
+        return  when(this){
+            ENKEL -> 2
+            MIDDELS -> 3
+            KRVENDE -> 4
+        }
+    }
+
+    fun getHoyde() : Int{
+        return antallkort / getHoyde()
+    }
+
+
+
+
+}
--- app/src/main/java/com/example/unomemo/spillKort/SpillKort.kt
+++ app/src/main/java/com/example/unomemo/spillKort/SpillKort.kt
@@ -1,17 +1,21 @@
 package com.example.unomemo.spillKort
 
 import android.os.Bundle
+import android.util.Log
 import android.view.*
 import androidx.fragment.app.Fragment
 import android.widget.TextView
-import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.unomemo.spillData.Vanskelighetsgrad
 import com.example.unomemo.R
 import com.example.unomemo.databinding.FragmentSpillKortBinding
+import com.example.unomemo.spillData.KortInfo
+import com.example.unomemo.spillData.START_FLAGS
 
 
 class SpillKort : Fragment() {
+
     private lateinit var recyc: RecyclerView
     private lateinit var parTextView: TextView
     private lateinit var bevegTextView: TextView
@@ -21,6 +25,26 @@ class SpillKort : Fragment() {
 
     private lateinit var spilAdapter :SpillBrettAdatper
 
+    private var brettSto : Vanskelighetsgrad = Vanskelighetsgrad.MIDDELS
+
+    private var kortInfo:List<KortInfo>? =null
+   // private val _kortInfo get() =  kortInfo
+
+    private var index :Int? =  null
+
+    private var antPar =  0;
+
+
+
+
+
+    companion object{
+        private const val  TAG  = "SpillKort"
+    }
+
+    interface Click{
+        fun onCardClicked(pos :Int)
+    }
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         _binding = FragmentSpillKortBinding.inflate(inflater,container,false)
@@ -30,11 +54,27 @@ class SpillKort : Fragment() {
         parTextView =  binding.antallPar
         bevegTextView =  binding.antallBevegelser
 
-        spilAdapter =  SpillBrettAdatper(requireContext(),8)
+        recyc.addItemDecoration(DefualtDecorator(15,15))
+
+        val flags:List<Int> =  START_FLAGS.shuffled().take(antallPar())
+
+        val shuffleIMG :List<Int> = (flags + flags).shuffled()
+
+        // lag ny liste av shuffleIMG og send det in til kort info data
+        kortInfo = shuffleIMG.map { KortInfo(it) }
+
+        spilAdapter =  SpillBrettAdatper(requireContext(),brettSto,kortInfo!!, object : Click{
+            override fun onCardClicked(pos: Int) {
+                Log.i(TAG,"$pos")
+                oppAdapter(pos)
+            }
+
+        })
+
 
         recyc.adapter = spilAdapter  // adapter provides a binding for the data set to the views of the recycler view
         recyc.setHasFixedSize(true)
-        recyc.layoutManager = GridLayoutManager(this.context,2)
+        recyc.layoutManager = GridLayoutManager(this.context,brettSto.getBredde())
 
         setHasOptionsMenu(true)
 
@@ -49,6 +89,71 @@ class SpillKort : Fragment() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return super.onOptionsItemSelected(item)
     }
+
+        // 8 kort 4 svar
+        // 12 kort  6  svar
+        //16 kort 8 svar
+
+    fun antallPar() : Int{
+        return  brettSto.antallkort / 2
+
+    }
+
+
+
+    private fun oppAdapter(pos:Int){
+
+        flipKort(pos)
+        // informing the adapter about changes
+        spilAdapter.run { notifyDataSetChanged() }
+    }
+
+
+    private fun flipKort(pos: Int){
+        val kort  = kortInfo?.get(pos)
+
+        if(index == null){
+            // this means they were either  0 cards previosuly flipped over
+                // or there were 2 cards previously flipped over and they are not matched
+                    // trun them back again by seting isup to false
+                        // and update the index to the postion taht is clicked
+            restKort();
+            index = pos;
+
+        } else {
+                check(index!!,pos)
+            index=null;
+        }
+        kort!!.isUp = !kort.isUp
+    }
+
+
+    private fun check(pos1:Int,pos2:Int) :Boolean {
+        val idIfno1  =  kortInfo!![pos1].id
+        val idIfno2 = kortInfo!![pos2].id
+
+        if(kortInfo!![pos1].id != kortInfo!![pos2].id){
+            Log.i(TAG, "$idIfno1 $idIfno2")
+            return false;
+        }
+        kortInfo!![pos1].isaMatch =  true;
+        kortInfo!![pos2].isaMatch =  true;
+        antPar++;
+        print( "this is antal kort $antPar" )
+        return true
+
+    }
+
+
+    private fun restKort() {
+        for (i in kortInfo!! ){
+            if(!i.isaMatch){
+                i.isUp= false;
+            }
+        }
+    }
+
+
 
 
 }
