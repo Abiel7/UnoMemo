d4a91a8b349e646f1c595236a2737c525ef0137c
milcaunicorn <70134656+milcaunicorn@users.noreply.github.com>
2021-10-10 21:28:32 +0200

    full navigation

--- app/src/main/java/com/example/unomemo/KatgoryFragment.kt
+++ app/src/main/java/com/example/unomemo/KatgoryFragment.kt
@@ -1,19 +1,19 @@
 package com.example.unomemo
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.NavHostFragment.findNavController
-import androidx.navigation.fragment.findNavController
-import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.unomemo.databinding.FragmentKatgoryBinding
 import com.example.unomemo.kategori.KategoriListeAdapter
-import kotlin.properties.Delegates
+
 
 
 class KatgoryFragment() : Fragment() {
@@ -30,30 +30,73 @@ class KatgoryFragment() : Fragment() {
                 container,
                 false
             )
-
         val recyclerView: RecyclerView = binding.kategoriRecyclerView
         val listAdapter = KategoriListeAdapter()
 
         recyclerView.adapter = listAdapter
         val layoutManager = GridLayoutManager(activity, 2)
         recyclerView.layoutManager = layoutManager
-        setHasOptionsMenu(true)
         return binding.root
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
 
-        val args:KatgoryFragmentArgs by navArgs()
-        var navControl = findNavController(this)
+    override fun onAttach(context: Context) {
+        //TODO check for login
+        super.onAttach(context)
+        var sharedPreferences = context
+            .getSharedPreferences("prefs",Context.MODE_PRIVATE)
+        var value = sharedPreferences.all["splashScreenPlayed"]
+            .toString() ?.toBooleanStrictOrNull() ?: nullErr(
+            sharedPreferences.all["splashScreenPlayed"].toString()
+        )
+        if(!value){
+            findNavController(this).navigate(KatgoryFragmentDirections.actionKatgoryFragmentToEntryPointFragment())
+            sharedPreferences.edit().putBoolean("splashScreenPlayed", true).commit()
+        }
 
-        if(!args.splashScreenShowed){
-            navControl.navigate(KatgoryFragmentDirections.actionKatgoryFragmentToEntryPointFragment())
+        //for debugging og feilsøking TODO sanitize
+        Log.v(
+            "KategoryFragment:onAttach; context.getSharedPrefrense().all['splashScreenPlayed']",
+            "value: $value"
+        )
+    }
+
+    override fun onResume() {
+        super.onResume()
+        //TODO check for login
+
+
+        var sharedPreferences = requireContext()
+            .getSharedPreferences("prefs",Context.MODE_PRIVATE)
+        var showedLoginFirstTime = sharedPreferences.all["showedLoginFirstTime"]
+            .toString() ?.toBooleanStrictOrNull() ?: nullErr(
+            sharedPreferences.all["showedLoginFirstTime"].toString()
+
+        )
+        if (!showedLoginFirstTime) {
+            findNavController(this).navigate(
+                KatgoryFragmentDirections.actionKatgoryFragmentToLoginFragment()
+            )
+            sharedPreferences.edit().putBoolean("showedLoginFirstTime", true).commit()
         }
-        if(!args.selfNavigated) {
-            navControl.navigate(KatgoryFragmentDirections.actionKatgoryFragmentSelf(true))
-            navControl.popBackStack(R.id.katgoryFragment, true)
-        }
+
+        //for debugging og feilsøking TODO sanitize
+        Log.v(
+            "KategoryFragment:onAttach; requireContext().getSharedPrefrense().all['showedLoginFirstTime']",
+            "value: $showedLoginFirstTime"
+        )
+
+    }
+
+    // for debugging og feilsøking
+    private fun nullErr(s :String?): Boolean {
+        Log.e(
+            "KategoryFragment",
+            "getSharedPrefrences()"
+                    + " null is not a valid value in"
+                    + " this context, try a valid key",
+            Throwable("invalid key, input a key that is defined"))
+        return true
     }
 
 }
