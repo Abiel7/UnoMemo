19ef905d5cf768b9f18a81be818b933ae3712623
abiel <56592834+Abiel7@users.noreply.github.com>
2021-10-27 18:19:51 +0200

    Spill Adapter

--- app/src/main/java/com/example/unomemo/spillKort/SpillBrettAdatper.kt
+++ app/src/main/java/com/example/unomemo/spillKort/SpillBrettAdatper.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
+import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.example.unomemo.spilldata.Vanskelighetsgrad
 import com.example.unomemo.R
@@ -15,13 +17,13 @@ import com.example.unomemo.spilldata.KortInfo
 class SpillBrettAdatper(
     private val spillFragment: Context,
     private val i: Vanskelighetsgrad,
-    private val infoKort: List<KortInfo>,
+    private val infoCard: List<KortInfo>,
     private val listner : SpillKort.Click
 ) : RecyclerView.Adapter<SpillBrettAdatper.ViewHolder>() {
 
-        companion object{
-            private const val TAG = "SpillBrettAdatper"
-        }
+    companion object{
+        private const val TAG = "SpillBrettAdatper"
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(spillFragment).inflate(R.layout.spillkortlayout,parent,false)
@@ -41,17 +43,19 @@ class SpillBrettAdatper(
     inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
         private val imageButton = itemView.findViewById<ImageButton>(R.id.imageButton)
 
-        fun bind(pos:Int){
 
-            if(infoKort[pos].isUp){
-                imageButton.setImageResource(infoKort[pos].id)
+
+        fun bind(pos:Int){
+            val  infoCard = infoCard[pos]
+            if(infoCard.isUp){
+                imageButton.setImageResource(infoCard.id)
             }
 
             else{
 
                 imageButton.setImageResource(R.drawable.ic_baseline_image_24)
-
             }
+
             imageButton.setOnClickListener {
 
                 Log.i(TAG, "Clicked on position $pos")
