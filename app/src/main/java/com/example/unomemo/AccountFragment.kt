12348caa5156270f3b919c63686677a90a3b954a
Kim Andre Undal <kim-undal@hotmail.com>
2021-11-06 21:16:56 +0100

    fixing av duplikate linjer

--- app/src/main/java/com/example/unomemo/AccountFragment.kt
+++ app/src/main/java/com/example/unomemo/AccountFragment.kt
@@ -13,7 +13,6 @@ import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.DocumentSnapshot
 
-import com.google.firebase.auth.FirebaseAuth
 
 import com.google.firebase.firestore.FirebaseFirestore
 
@@ -60,3 +59,4 @@ class AccountFragment : Fragment() {
                 }
             }
     }
+}
